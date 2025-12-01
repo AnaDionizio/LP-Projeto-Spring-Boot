96d755270322625fe8a0cea561e1280058a33e38
AnaDionizio <ajulya.rd@gmail.com>
2025-12-01 02:17:34 -0300

    Add ProdutoController for product management

--- AdocaoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bemestaranimal.repository;
-
-import com.example.bemestaranimal.entity.Adocao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdocaoRepository extends JpaRepository<Adocao, Long> {
-}
